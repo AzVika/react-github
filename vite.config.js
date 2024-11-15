2527074a7245139277b006954c5eeda9ef4f7993
AzVika <26838569+AzVika@users.noreply.github.com>
2024-11-15 12:19:06 +0200

    edit settings vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/react-github',
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern-compiler' // or "modern"
+			}
+		}
+	},
+	base: '/react-github/',
 })
