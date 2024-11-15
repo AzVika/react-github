1dea31044ffa0a1919b025ee7fb5c80f471e9dd4
AzVika <26838569+AzVika@users.noreply.github.com>
2024-11-15 12:02:43 +0200

    add settings base url

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename='/react-github/'>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>,
